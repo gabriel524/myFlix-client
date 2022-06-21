57b888221a2abf727d599626384a35b8fcb0079e
Gabriel Abraham. Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-06-21 12:56:08 +0200

    prop types and onclick for movie card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class MovieCard extends React.Component {
-  render() {
-    const { movie, onMovieClick } = this.props;
-
-    return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
-  }
+  
+//Basic display of movies that are rendered on MainView
+render() {
+  const { movie, onMovieClick } = this.props;
+  return (
+    <div
+      className="movie-card"
+      onClick={() => {
+        onMovieClick(movie);
+      }}
+    >
+      {movie.Title}
+    </div>
+  );
 }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    ReleaseYear: PropTypes.number.isRequired,
+    Description: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired
+    }),
+    ImagePath: PropTypes.string.isRequired
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
