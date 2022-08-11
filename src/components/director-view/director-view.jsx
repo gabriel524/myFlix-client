96bbf12eb749f428dba813f5fa408619b27690d7
Gabriel Abraham. Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-08-11 03:08:37 +0200

    major update to directory view file

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -4,25 +4,28 @@ import { MovieCard } from '../movie-card/movie-card';
 
 export class DirectorView extends React.Component {
   render() {
-    const { director, directorMovies, goBack } = this.props;
+    const { director, handleClick } = this.props;
+    
+    console.log('director', director)
     return (
       <Container className="mt-5">
-        <h1>{director.name} </h1>
-        <p>―Born in {director.birthYear}―</p>
-        <Button className="mb-4" variant="warning" onClick={goBack}>
+        <h1>{director.Name} </h1>
+        <p className='textcolor'>―Born in {director.BirthYear}―</p>
+        <a href onClick={handleClick} className='btn btn-warning'>
           « Back
-        </Button>
+        </a>
         <h2 className="subtitle">BIO: </h2>
-        <p>{director.bio}</p>
+        <p>{director.Bio}</p>
         <h2 className="subtitle">DIRECTED MOVIES: </h2>
         <Row className="justify-content-center mt-3">
-          {directorMovies.map((movie) => (
-            <MovieCard key={movie._id} movie={movie}>
-              {movie.title}
-            </MovieCard>
-          ))}
+        {/*  {director && director.movies.map((movie) => (*/}
+        {/*    <MovieCard key={movie._id} movie={movie}>*/}
+        {/*      {movie.title}*/}
+        {/*    </MovieCard>*/}
+        {/*  ))}*/}
         </Row>
       </Container>
     );
   }
 }
+
