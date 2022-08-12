a75ad59463d64cb6c0138393cbd47d8d0af43f49
Gabriel Abraham. Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-08-12 15:53:03 +0200

    link import remove from code

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
-import { Link } from "react-router-dom";
+
 
 import { Container, Row, Col, Card, Navbar, Nav, Form, Button, NavDropdown } from 'react-bootstrap';
 
