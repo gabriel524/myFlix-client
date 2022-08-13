14636549bb60de0ddc3660bc6ec16a466f486168
Gabriel Abraham. Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-08-13 20:02:26 +0200

    code update in director view file

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Container, Row, Button } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 import { MovieCard } from '../movie-card/movie-card';
 
 export class DirectorView extends React.Component {
   render() {
-    const { director, goback } = this.props;
+    const { director, onBackClick } = this.props;
     
     console.log('director', director)
     return (
       <Container className="mt-5">
         <h1>{director.Name} </h1>
         <p className='textcolor'>―Born in {director.BirthYear}―</p>
-        <a href onClick={goback} className='btn btn-warning'>« Back</a>
+        <a href onClick={() => { onBackClick(null); }} className='btn btn-warning'>« Back</a>
         <h2 className="subtitle">BIO: </h2>
         <p>{director.Bio}</p>
         <h2 className="subtitle">DIRECTED MOVIES: </h2>
@@ -26,4 +27,8 @@ export class DirectorView extends React.Component {
     );
   }
 }
+
+DirectorView.propTypes = {
+  onBackClick: PropTypes.func.isRequired
+};
 
